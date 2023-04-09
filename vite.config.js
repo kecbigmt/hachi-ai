8ef8320fcc353cc92a9234ccdb75b2315c1cea33
kecy <kec.bigmt@gmail.com>
2023-04-10 04:26:51 +0900

    chore: setup emotion

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,7 @@ export default defineConfig({
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh({
+    jsxImportSource: '@emotion/react',
+  })],
 })
