8ee73a118dcd347f6390a2f5d471937478927d82
kecy <kec.bigmt@gmail.com>
2023-04-09 19:43:50 +0900

    feat: implement voice recognition into frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { invoke } from '@tauri-apps/api'
+import { open } from '@tauri-apps/api/dialog';
 import { useState } from 'react';
 
 import { getAudioStream, startRecording, stopRecording } from './utils/audioInput';
@@ -6,35 +7,66 @@ import { getAudioStream, startRecording, stopRecording } from './utils/audioInpu
 export const App = () => {
   const [inputText, setInputText] = useState("");
   const [responseText, setResponseText] = useState("");
+  const [modelFilePath, setModelFilePath] = useState<string | undefined>(undefined);
+  const [modelLoaded, setModelLoaded] = useState(false);
+
+  const handleSelectFile = async () => {
+    const filePath = await open();
+    if (!filePath) return;
+    if (Array.isArray(filePath) && filePath.length === 0) return;
+
+    const modelFilePath = Array.isArray(filePath) ? filePath[0] : filePath;
+
+    invoke('load_model', { modelFilePath }).then(() => {
+      setModelLoaded(true);
+      setModelFilePath(Array.isArray(filePath) ? filePath[0] : filePath);
+    });
+  };
 
   const handleAudioInput = async (): Promise<void> => {
     const stream = await getAudioStream();
     if (!stream) return;
 
     startRecording(stream);
+  };
 
-    setTimeout(async() => {
-      const audioBlob = await stopRecording();
-      const audioUrl = URL.createObjectURL(audioBlob);
-      const audio = new Audio(audioUrl);
-      audio.play();
-    }, 5000);
+  const handleAudioInputStop = async (): Promise<void> => {
+    const audioData = Array.from(await stopRecording());
+
+    invoke<string>('transcribe_audio', { audioData }).then((res) => {
+      console.log(`transcribe_audio: ${res}`);
+      setInputText(res);
+    });
   };
 
   return (
     <div className="App">
       <h1>ChatGPT音声アシスタント</h1>
-      <button onClick={handleAudioInput}>音声入力</button>
-      <div className="container">
-        <div className="inputText">
-          <h2>あなたの質問:</h2>
-          <p>{inputText}</p>
-        </div>
-        <div className="responseText">
-          <h2>ChatGPTの回答:</h2>
-          <p>{responseText}</p>
-        </div>
-      </div>
+      <button onClick={handleSelectFile}>モデルファイルを選択</button>
+      {modelFilePath && (
+        <>
+          <p>ロード済み: {modelFilePath}</p>
+        </>
+      )}
+      {
+        modelLoaded && (
+          <>
+            <button disabled={!modelFilePath} onClick={handleAudioInput}>音声入力</button>
+            <button disabled={!modelFilePath} onClick={handleAudioInputStop}>ストップ</button>
+            <div className="container">
+              <div className="inputText">
+                <h2>あなたの質問:</h2>
+                <p>{inputText}</p>
+              </div>
+              <div className="responseText">
+                <h2>ChatGPTの回答:</h2>
+                <p>{responseText}</p>
+              </div>
+            </div>
+          </>
+        )
+      }
+
     </div>
   );
 }
--- src/utils/audioInput.ts
+++ src/utils/audioInput.ts
@@ -1,9 +1,15 @@
 let mediaRecorder: MediaRecorder;
 const recordedChunks: BlobPart[] = [];
+const audioContext = new window.AudioContext({ sampleRate: 16000 }); // whisper-rs expects 16bit, 16KHz, mono audio
 
 export const getAudioStream = async (): Promise<MediaStream | null> => {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: {
+            // whisper-rs expects 16bit, 16KHz, mono audio
+            channelCount: 1,
+            sampleRate: 16000,
+            sampleSize: 16,
+        } });
         return stream;
     } catch (error) {
         console.error("Error accessing the microphone:", error);
@@ -21,12 +27,20 @@ export const startRecording = (stream: MediaStream): void => {
     mediaRecorder.start();
 };
 
-export const stopRecording = (): Promise<Blob> => {
+export const stopRecording = (): Promise<Float32Array> => {
     return new Promise((resolve) => {
         mediaRecorder.addEventListener("stop", () => {
             const blob = new Blob(recordedChunks, { type: "audio/webm" });
             recordedChunks.length = 0;
-            resolve(blob);
+            
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(blob);
+            reader.onload = () => {
+                audioContext.decodeAudioData(reader.result as ArrayBuffer).then((audioBuffer) => {
+                    const audioData = audioBuffer.getChannelData(0);
+                    resolve(audioData);
+                });
+            }
         });
         mediaRecorder.stop();
     });
