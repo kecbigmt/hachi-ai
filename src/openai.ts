a2837f80f3c7d9e1287e630869c0b1ecddbadc3a
kecy <kec.bigmt@gmail.com>
2023-04-10 23:03:46 +0900

    feat: add openai util

---
+++ src/openai.ts
@@ -0,0 +1,11 @@
+import { invoke } from '@tauri-apps/api';
+import { Configuration, OpenAIApi } from 'openai';
+
+// get OPENAI_API_KEY from backend
+const getOpenAIAPIKey = async () => invoke<string>('get_openai_api_key', {});
+
+const configuration = new Configuration({
+  apiKey: await getOpenAIAPIKey(),
+});
+
+export const openai = new OpenAIApi(configuration);
